64389be94832f74a154423add26dc0e455b04f8e
Chris Andrews <christopher.m.andrews@gmail.com>
2015-03-30 21:34:06 -0500

    Replicated other GUI examples
    Replicated other GUI example code to test js-csp

--- csp-example.js
+++ csp-example.js
@@ -1,3 +1,53 @@
 var csp = require('js-csp');
 
-console.log('hello world');
+window.onload = function() {
+  init();
+};
+
+init = function() {
+
+  function listen(el, type) {
+    var ch = csp.chan();
+    el.addEventListener(type, function(e) {
+      csp.putAsync(ch, {evt:e, el:el});
+    });
+    return ch;
+  }
+
+  csp.go(function*() {
+    var el = document.getElementById('el1');
+    var mousech = listen(el,'mousemove');
+    var clickch = listen(el,'click');
+    //clickch = csp.operations.merge([mousech,clickch]);
+    var mousePos = [0,0];
+    var clickPos = [0,0];
+    while (true) {
+      var v = yield csp.alts([mousech,clickch]);
+      var e = v.value.evt;
+      if (v.channel === mousech) {
+        mousePos = [e.clientX, e.clientY];
+      }
+      else {
+        clickPos = [e.clientX, e.clientY];
+      }
+      el.innerHTML = (mousePos[0] + ', ' + mousePos[1] + ' - ' +
+        clickPos[0] + ', ' + clickPos[1]);
+    }
+  });
+
+};
+
+
+// csp.go(function*() {
+//   var val;
+//   while((val = yield csp.take(ch)) !== csp.CLOSED) {
+//     console.log(val);
+//   }
+// });
+//
+// csp.go(function*() {
+//   yield csp.put(ch, 1);
+//   yield csp.take(csp.timeout(1000));
+//   yield csp.put(ch, 2);
+//   ch.close();
+// });
